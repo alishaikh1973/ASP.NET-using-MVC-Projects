75917571fb456220b3402c64f5c297b6214ae5d4
ALI SHAIKH 1973 <139847421+alishaikh1973@users.noreply.github.com>
2023-10-24 03:21:54 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,82 +1,162 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LINQtoObjectDemo
+namespace LINQAssessment
 {
-    public class Employee
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string City { get; set; }
-        public int Salary { get; set; }
+        public int Price { get; set; }
+        public string CompanyName { get; set; }
 
-    }
-    internal class Program
-    {
         static void Main(string[] args)
         {
-            List<Employee> Employees = new List<Employee>()
+            List<Product> products = new List<Product>()
             {
-                new Employee{Id=1,Name="Rohan",City="Pune",Salary=25000},
-                new Employee{Id=2,Name="Kishor",City="Mumbai",Salary=26000},
-                new Employee{Id=3,Name="Tushar",City="Nagpur",Salary=23000},
-                new Employee{Id=4,Name="Amol",City="Pune",Salary=28000},
-                new Employee{Id=5,Name="Trupti",City="Satara",Salary=22000},
-                new Employee{Id=6,Name="Pradnya",City="Pune",Salary=26000},
-                new Employee{Id=7,Name="Payal",City="Mumbai",Salary=29000},
-                new Employee{Id=8,Name="Suraj",City="Nashik",Salary=27000},
-                new Employee{Id=9,Name="Rohit",City="Solapur",Salary=21000},
-                new Employee{Id=10,Name="Sandesh",City="Nagpur",Salary=22000},
-                new Employee{Id=11,Name="Ananta",City="Pune",Salary=22500},
+                new Product{Id=1,Name="Mouse",Price=799,CompanyName="Dell"},
+                new Product{Id=2,Name="Mouse",Price=699,CompanyName="Lenovo"},
+                new Product{Id=3,Name="Laptop",Price=34799,CompanyName="Dell"},
+                new Product{Id=4,Name="Laptop",Price=45799,CompanyName="Sony"},
+                new Product{Id=5,Name="Laptop",Price=38799,CompanyName="Lenovo"},
+                new Product{Id=6,Name="Keyboard",Price=599,CompanyName="Dell"},
+                new Product{Id=7,Name="Keyboard",Price=999,CompanyName="Microsoft"},
+                new Product{Id=8,Name="RAM 4GB",Price=2799,CompanyName="Corsair"},
+                new Product{Id=9,Name="Speaker",Price=5799,CompanyName="Sony"},
+                new Product{Id=10,Name="USB Mouse",Price=1299,CompanyName="Microsoft"},
             };
 
 
-            var result = from e in Employees
-                         where e.City == "Pune"
-                         select e;
+             // Using LINQ Query:-
+
+              // 1. Display all products using LINQ query
+                 var result1 = from p in products select p;
+                 /* foreach (Product item in result1)
+                  {
+                 Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+                  }*/
 
 
-            //foreach (Employee item in result)
-            //{
-            //    Console.WriteLine($"{item.Id}, {item.Name},{item.City},{item.Salary}");
-            //}
-
-
-            var result2 = from e in Employees
-                          where e.Name.StartsWith("P")
-                          select e;
-
-
-            //foreach (Employee item in result2)
-            //{
-            //    Console.WriteLine($"{item.Id}, {item.Name},{item.City},{item.Salary}");
-            //}
-
-
-            var result3 = from e in Employees
-                          where e.Salary > 24000 && e.City == "Pune"
-                          select e;
-
-
-            //foreach (Employee item in result3)
-            //{
-            //    Console.WriteLine($"{item.Id}, {item.Name},{item.City},{item.Salary}");
-            //}
-
-
-            //var result4 = Employees.Where(x => x.City == "Mumbai").ToList();
-            var result4 = Employees.Where(x => x.City == "Pune").OrderBy(x => x.Salary).ToList();
-            foreach (Employee item in result4)
+            //2.Display products whose price is greater than 1500
+            var result2 =  from p in products where p.Price>1500 select p;
+            foreach (Product item in result2)
             {
-                Console.WriteLine($"{item.Id}, {item.Name},{item.City},{item.Salary}");
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
             }
 
 
-        }
+            //3.Display products whose price is in between 10000 to 40000
+            var result3 = from p in products where p.Price >= 10000 && p.Price<=40000 select p;
+            foreach (Product item in result3)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
 
+            //4.Display products of Dell company
+            var result4 = from p in products where p.CompanyName == "Dell" select p;
+            foreach (Product item in result4)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+            //5.Display all company laptops
+            var result5 = from p in products where p.Name == "laptop" select p;
+            foreach (Product item in result5)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+            //6.Display products whose company name start with ‘M’
+            var result6 = from p in products where p.CompanyName.StartsWith("M") select p;
+            foreach (Product item in result6)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+            //7.Display all company mouse whose price is less than 1000
+           var result7 = from p in products
+            where (p.Name == "Mouse" &&
+                  (p.CompanyName == "Microsoft" ||
+                   p.CompanyName == "Lenovo") &&
+                   p.Price < 1000)
+            select p;
+            foreach (Product item in result7)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+
+
+            //8.Display all products descending order by their price
+            var result8 = from p in products orderby p.Price descending select p;
+            foreach (Product item in result8)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+
+            //9.Display all products accessing order by their company name
+            var result9 = from p in products orderby p.CompanyName ascending select p;
+            foreach (Product item in result9)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+            //10.Display all keyboards accessing order by their price
+            var result10 = from p in products where p.Name=="Keyboard" orderby p.Price ascending select p;
+            foreach (Product item in result10)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+
+            // Using Lambda Expression:-
+
+            //1.Display all products descending order by their price
+            var result11 = products.OrderByDescending(product => product.Price);
+            foreach (Product item in result11)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+
+            //2.Display product whose Id is 5
+            var result12 = products.Where(product => product.Id == 5);
+            foreach (Product item in result12)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+            //3.Display all products whose price less than 5000
+            var result13 = products.Where(product => product.Price <= 5000);
+            foreach (Product item in result13)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+            //4.Display 3 products which have maximum price
+            var result14 = products.OrderByDescending(product=> product.Price).Take(3);
+            foreach (Product item in result14)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+
+            //5.Display 5 products which have minimum price
+            var result15 = products.OrderBy(product => product.Price).Take(5);
+            foreach (Product item in result15)
+            {
+                Console.WriteLine($"{item.Id}, {item.Name},{item.Price},{item.CompanyName}");
+            }
+
+
+
+
+
+        }
     }
 }
-
